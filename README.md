06c01b0ab6c744c32d9c853a5fe39d4c8edd405e
chris <chris@afox.cc>
2022-06-29 22:10:27 +0800

    Added readme

---
+++ README.md
@@ -0,0 +1,15 @@
+# 感谢
+
+[WTFSolidity](https://github.com/AmazingAng/WTFSolidity) 是我很喜欢的solidity教程，但是一直切换网页查看会比较麻烦，所以我想写一个简单的网站，让我可以在线查看。
+
+参考了 [boboidream/note](https://github.com/boboidream/note)  实现了将网页使用vuepress直接编译生成静态页面的功能。
+
+使用 https://vercel.com/ 进行部署
+
+# 使用
+
+```
+git clone git@github.com:EasyChris/WTFSolidity-web.git
+chmod +x deploy.sh 
+./deploy.sh
+```
--- deploy.sh
+++ deploy.sh
@@ -8,20 +8,5 @@ git clone git@github.com:AmazingAng/WTFSolidity.git
 
 mv -f WTFSolidity/* docs/
 
-# build
-npm run build
+rm -rf WTFSolidity
 
-# navigate into the build output directory
-cd docs/.vuepress/dist
-
-# if you are deploying to a custom domain
-# echo 'note.wenboz.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:boboidream/note.git master
-
-# cd -
